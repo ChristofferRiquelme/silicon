05201d8040aa9a05d12c3f1b06d74918b67e2306
ChristofferRiquelme <christoffer.riquelme@gmail.com>
2025-10-05 19:32:08 +0200

    Add dark mode

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
+document.getElementById('darkmode-switch').addEventListener('change', function() {
+  if (this.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+});
+
+const darkModeText = document.querySelector('.silicon-navbar-actions-darkmode-text');
+const darkModeSwitch = document.getElementById('darkmode-switch');
+
+darkModeSwitch.addEventListener('change', function() {
+  if (this.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    darkModeText.textContent = 'Light Mode';
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    darkModeText.textContent = 'Dark Mode';
+  }
+});
+
+
 var icons = document.getElementsByClassName("silicon-faq-accordion-icon");
 for (var i = 0; i < icons.length; i++) {
   icons[i].addEventListener("click", function(event) {
     var accordionBtn = this.closest(".silicon-faq-accordion");
     var panel = accordionBtn.nextElementSibling;
 
-    // Om redan öppen: stäng den
     var isOpen = accordionBtn.classList.contains("active");
     if (isOpen) {
       accordionBtn.classList.remove("active");
       if (panel) panel.classList.remove("open");
     } else {
-      // Stäng alla andra
+
       var allAccordions = document.getElementsByClassName("silicon-faq-accordion");
       var allPanels = document.getElementsByClassName("silicon-faq-accordion-panel");
       for (var j = 0; j < allAccordions.length; j++) {
@@ -19,7 +40,7 @@ for (var i = 0; i < icons.length; i++) {
       for (var k = 0; k < allPanels.length; k++) {
         allPanels[k].classList.remove("open");
       }
-      // Öppna den du klickade på
+
       accordionBtn.classList.add("active");
       if (panel) panel.classList.add("open");
     }
